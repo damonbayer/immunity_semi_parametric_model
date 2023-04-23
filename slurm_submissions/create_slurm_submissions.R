f267544659dbd22748b6a21ca155beec6bfa74da
Damon Bayer <damon@damonbayer.com>
2023-04-23 12:54:26 -0700

    slurm util

---
+++ slurm_submissions/create_slurm_submissions.R
@@ -0,0 +1,21 @@
+slurm_dir <- "slurm_submissions/simulation/compare_inference_unknown_variant_proportion_ihr_contact/"
+
+tibble(file_path = dir_ls(slurm_dir, recurse = 1, type = "file")) %>% 
+  filter(path_file(file_path) == "fit_models.sh") %>% 
+  pull(file_path) %>% 
+  str_c("sbatch ", .) %>% 
+  cat(sep = "\n")
+
+
+tibble(file_path = dir_ls(slurm_dir, recurse = 1, type = "file")) %>% 
+  filter(path_file(file_path) == "generate_predictive_and_generated_quantities.sh") %>% 
+  pull(file_path) %>% 
+  str_c("sbatch ", .) %>% 
+  cat(sep = "\n")
+
+
+tibble(file_path = dir_ls(slurm_dir, recurse = 1, type = "file")) %>% 
+  filter(path_file(file_path) == "tidy_results.sh") %>% 
+  pull(file_path) %>% 
+  str_c("sbatch ", .) %>% 
+  cat(sep = "\n")
