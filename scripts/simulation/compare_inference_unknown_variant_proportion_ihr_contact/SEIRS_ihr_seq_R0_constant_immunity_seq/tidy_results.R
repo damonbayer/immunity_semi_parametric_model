2dfc8d2bf87d6fe672883f08f2438e7ab10bfea5
Damon Bayer <damon@damonbayer.com>
2023-04-26 15:21:08 -0700

    compute scores using mixture of parameters

--- scripts/simulation/compare_inference_unknown_variant_proportion_ihr_contact/SEIRS_ihr_seq_R0_constant_immunity_seq/tidy_results.R
+++ scripts/simulation/compare_inference_unknown_variant_proportion_ihr_contact/SEIRS_ihr_seq_R0_constant_immunity_seq/tidy_results.R
@@ -59,10 +59,38 @@ tidy_prior_predictive <- tidy_predictive(prior_predictive) %>%
   mutate(weeks_ahead = time - target_max_t)
 tidy_posterior_predictive <- tidy_predictive(posterior_predictive) %>%
   mutate(weeks_ahead = time - target_max_t)
+# Score -------------------------------------------------------------------
+phi_draws <-
+  posterior_generated_quantities %>%
+  select(starts_with("."), starts_with("ϕ_")) %>%
+  tidy_format_draws_time() %>%
+  select(-time) %>%
+  mutate(param_type = "phi") %>%
+  mutate(target_type = name %>%
+           str_remove_all("ϕ_") %>%
+           if_else(. == "new_seq", "new_seq_variant_1", .)) %>%
+  select(-starts_with("name")) %>%
+  pivot_wider(names_from = param_type, values_from = value) %>%
+  bind_rows(., filter(., target_type == "new_seq_variant_1") %>% mutate(target_type = "new_seq_variant_2")) %>%
+  arrange(.draw, target_type) %>%
+  select(sample = .draw, target_type, phi) %>% mutate(phi = if_else(phi > 1e4, 1e4, phi))
+
+mean_draws <-
+  posterior_generated_quantities %>%
+  select(starts_with("."), matches("mean")) %>%
+  tidy_format_draws_time() %>%
+  filter(name != "new_seq_mean") %>%
+  mutate(param_type = "mean") %>%
+  mutate(target_type =  str_remove_all(name, "_mean")) %>%
+  select(-starts_with("name")) %>%
+  pivot_wider(names_from = param_type, values_from = value) %>%
+  select(sample = .draw, time, target_type, mean)
 
 
+mean_phi_draws <- left_join(mean_draws, phi_draws) %>%
+  mutate(target_type = str_c("data_", target_type))
 
-posterior_predictive_score <-
+posterior_predictive_score_setup <-
   posterior_predictive %>%
   tidy_format_draws_time() %>%
   select(sample = .draw, prediction = value, name, time) %>%
@@ -71,10 +99,28 @@ posterior_predictive_score <-
   left_join(dat_tidy) %>%
   mutate(max_t = target_max_t) %>%
   mutate(model = model_name) %>%
-  select(time, model, target_type = name, max_t, weeks_ahead, prediction, sample, true_value) %>%
-  score_with_override(override = "continuous")
+  select(time, model, target_type = name, max_t, weeks_ahead, prediction, sample, true_value)
 
+posterior_predictive_score_nbinom <-
+  posterior_predictive_score_setup %>%
+  left_join(mean_phi_draws) %>%
+  mutate(nbinom = pmap(
+    list(true_value, phi, mean),
+    ~c(
+      crps_nbinom = scoringRules::crps_nbinom(y = ..1, size = ..2, mu = ..3),
+      logs_nbinom = scoringRules::logs_nbinom(y = ..1, size = ..2, mu = ..3),
+      dss_nbinom = scoringRules::dss_nbinom(y = ..1, size = ..2, mu = ..3)))) %>%
+  unnest_wider(nbinom) %>%
+  group_by(time, model, target_type, max_t, weeks_ahead) %>%
+  summarize(across(matches("nbinom"), mean), .groups = "drop")
 
+posterior_predictive_score_kd <-
+  posterior_predictive_score_setup %>%
+  score_with_override(override = "continuous") %>%
+  as_tibble()
+
+posterior_predictive_score <- full_join(posterior_predictive_score_kd,
+                                        posterior_predictive_score_nbinom)
 dir_create(path(results_dir, "tidy_prior_generated_quantities"))
 dir_create(path(results_dir, "tidy_posterior_generated_quantities"))
 dir_create(path(results_dir, "tidy_prior_predictive"))
