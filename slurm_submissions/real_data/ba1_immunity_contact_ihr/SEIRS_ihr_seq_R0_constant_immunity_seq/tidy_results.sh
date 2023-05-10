9ecdb912f55b061b95d097b10edce120ad891b60
Damon Bayer <damon@damonbayer.com>
2023-05-09 17:43:24 -0700

    ba1_immunity_contact_ihr

---
+++ slurm_submissions/real_data/ba1_immunity_contact_ihr/SEIRS_ihr_seq_R0_constant_immunity_seq/tidy_results.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -p standard   ## run on the standard partition
+#SBATCH -A vminin_lab ## account to charge
+#SBATCH -N 1          ## run on a single node
+#SBATCH -n 1          ## request 1 task (1 CPUs)
+#SBATCH -t 01:00:00   ## 1 hr run time limit
+#SBATCH --mem=3G
+#SBATCH -o tidy_results-%A_%a.out
+#SBATCH --mail-type=begin,end
+#SBATCH --mail-user=bayerd@uci.edu
+#SBATCH --array=30-36
+
+module purge
+module load R
+cd //pub/bayerd/immunity_semi_parametric_model
+
+Rscript scripts/real_data/ba1_immunity_contact_ihr/SEIRS_ihr_seq_R0_constant_immunity_seq/tidy_results.R $SLURM_ARRAY_TASK_ID
---
+++ slurm_submissions/real_data/ba1_immunity_contact_ihr/SEIRS_ihr_seq_R0_seq_immunity_seq/fit_models.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -p standard   ## run on the standard partition
+#SBATCH -A vminin_lab ## account to charge
+#SBATCH -N 1          ## run on a single node
+#SBATCH -n 4          ## request 4 tasks (4 CPUs)
+#SBATCH -t 12:00:00   ## 2 hr run time limit
+#SBATCH --mem=16G
+#SBATCH -o fit_models-SEIRS_ihr_seq_R0_seq_immunity_seq-%A_%a.out
+#SBATCH --mail-type=begin,end
+#SBATCH --mail-user=bayerd@uci.edu
+#SBATCH --array=30-36
+#SBATCH --exclude=hpc3-15-29,hpc3-21-30,hpc3-21-23
+
+module purge
+module load julia/1.8.5
+cd //pub/bayerd/immunity_semi_parametric_model/
+
+if [ $SLURM_ARRAY_TASK_ID == 30 ]; then
+sbatch --depend=afterany:$SLURM_ARRAY_JOB_ID slurm_submissions/real_data/ba1_immunity_contact_ihr/SEIRS_ihr_seq_R0_seq_immunity_seq/generate_predictive_and_generated_quantities.sh
+fi
+
+julia --project --threads 4 scripts/real_data/ba1_immunity_contact_ihr/SEIRS_ihr_seq_R0_seq_immunity_seq/fit_model.jl $SLURM_ARRAY_TASK_ID
